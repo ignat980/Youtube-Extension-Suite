7cfaa8bb36eecdd5d1d01628272e435c439b7a8a
Ignat Remizov <ignat980@gmail.com>
2016-03-30 13:00:08 -0700

    Optimized script for performance and readability

--- js/main.js
+++ js/main.js
@@ -1,71 +1,10 @@
 // Copyright (c) 2016 Ignat Remizov. All rights reserved.
 ;(function(root, undefined) {
 /**
- * Calls the Youtube API to read the length of the current playlist
- *
- * @param {string} playlistID - The ID for the playlist to get
- * @param {string} key - The Youtube data v3 api key
- * @param {function(string)} callback - called when the length of a Youtube Playlist is found
- */
-function getPlaylistLength(playlist_ID, key, callback) {
-  console.log("Getting playlist length");
-  var playlist_api_url = "https://www.googleapis.com/youtube/v3/playlistItems" +
-  "?part=contentDetails&maxResults=50&playlistId=" + playlist_ID +
-  "&fields=etag%2Citems%2FcontentDetails%2CnextPageToken%2CprevPageToken&key=" + key;
-  // So after reading a lot, there are a lot of hoops to go through to get all of the items' durations.
-  // I have to call /v3/playlistItems to get videoId's, and I can only do 50 items at a time, so I have to keep track of a pageToken
-  // I then have to call /v3/videos with all video id's, and sum all the durations together
-  // Obviously this is a lot of time to process, so I guess I would have a load indicator or something, I wonder if I can use youtube's
-  var data = ["PT32H10M33S", "PT2M01S", "PT32M10S", "PT11M5S","PT22M10S"];
-  var j = asyncJsonGET(playlist_api_url, function(res) {
-    console.log(res);
-    // TODO: Call GET /v3/videos to get video information
-    // TODO: Convert video objects to what the data variable looks like
-    // TODO: Render length :D
-  }, function(err) {
-    console.error(err);
-  });
-  var length = data.reduce(function(previous, current) {
-    var duration = moment.duration(previous);
-    duration.add(moment.duration(current));
-    return duration;
-  });
-  callback(length);
-};
-
-/**
- * Formats a moment duration from milliseconds into a readable format
- *
- * @param {Duration} duration - a duration that will be used to formatDuration
- * @param {string} format_string - the string that will be used by .format() or you can use the two default values
- */
-function formatDuration(duration, format_string) {
-  console.log(duration);
-  var length;
-  if (format_string === "long") {
-    format = [
-      duration.months() === 1 ? "Month" : "Months",
-      duration.days() === 1 ? "Day" : "Days",
-      duration.hours() === 1 ? "Hour" : "Hours",
-      duration.minutes() === 1 ? "Minute" : "Minutes",
-      duration.seconds() === 1 ? "Second" : "Seconds"
-    ];
-    length = duration.format("M [" + format[0] + "] d [" + format[1] +
-    "] h [" + format[2] + "] m [" + format[3] + " and] s [" + format[4] + "]");
-  } else if (format_string === "short") {
-    length = duration.format("M[m] d[d] h:mm:ss");
-  } else {
-    length = duration.format(format_string);
-  };
-  return length;
-}
-
-/**
  * @param {string} url - The URL to get from
- * @param {function(Response)} callback - Called when the GET request finishes
- * @param {function(error)} errorCallback - Called when the request returns an error
+ * @param {function(Response)} callback - Called when the GET json request finishes
+ * @param {function(Error)} errorCallback - Called when the request fails or returns an error
  */
-
 function asyncJsonGET(url, callback, errorCallback) {
   var x = new XMLHttpRequest();
   x.open("GET", url);
@@ -107,15 +46,90 @@ function readTextFile(file, callback) {
   raw_file.send(null);
 };
 
+/**
+ * Reduces a bunch of ISO 8601 durations into a moment.Duration object
+ *
+ * @param {[string]} data - An array of ISO 8601 formatted strings
+ * @returns {Duration} - The sum of the ISO 8601 strings as a moment.Duration object
+ */
+function sumLengths(data) {
+  console.log("Summing together strings");
+  return data.reduce(function(previous, current) {
+    if(typeof previous === 'string') {
+      var duration = moment.duration(previous);
+    } else {
+      var duration = previous;
+    };
+    if (current) {
+      duration.add(moment.duration(current));
+    };
+    return duration;
+  });
+};
 
 /**
- * Adds a playlist length to the DOM
+ * Formats a moment.Duration object into a readable string
  *
- * @param {string} format_string - used by duration.format to format the length
- * @param {string} length - The length of the playlist as a moment.Duration
+ * @param {Duration} duration - a moment.Duration object that will be formatted
+ * @param {string} format_string - the string that will be used by .format() or you can use the two default values "long" or "short"
+ * @returns {string} - a formatted string from a Duration object using the moment-duration-format plugin
  */
-function renderLengthInDOM(length, format_string) {
-  var length = formatDuration(length, format_string);
+function formatDuration(duration, format_string) {
+  console.log("Formatting", duration);
+  var length;
+  if (format_string === "long") {
+    format = [
+      duration.months() === 1 ? "Month" : "Months",
+      duration.days() === 1 ? "Day" : "Days",
+      duration.hours() === 1 ? "Hour" : "Hours",
+      duration.minutes() === 1 ? "Minute" : "Minutes",
+      duration.seconds() === 1 ? "Second" : "Seconds"
+    ];
+    length = duration.format("M [" + format[0] + "] d [" + format[1] +
+    "] h [" + format[2] + "] m [" + format[3] + " and] s [" + format[4] + "]");
+  } else if (format_string === "short") {
+    length = duration.format("M[m] d[d] h:mm:ss");
+  } else {
+    length = duration.format(format_string);
+  };
+  return length;
+}
+
+/**
+ * Calls the Youtube API to read the length of the current playlist
+ *
+ * @param {string} playlistID - The ID for the playlist to get
+ * @param {string} key - The Youtube data v3 api key
+ * @param {function(string)} callback - called when the length of a Youtube Playlist is parsed
+ */
+function getPlaylistLength(playlist_ID, key, callback) {
+  console.log("Getting playlist length");
+  var playlist_api_url = "https://www.googleapis.com/youtube/v3/playlistItems" +
+  "?part=contentDetails&maxResults=50&playlistId=" + playlist_ID +
+  "&fields=etag%2Citems%2FcontentDetails%2CnextPageToken%2CprevPageToken&key=" + key;
+  // So after reading a lot, there are a lot of hoops to go through to get all of the items' durations.
+  // I have to call /v3/playlistItems to get videoId's, and I can only do 50 items at a time, so I have to keep track of a pageToken
+  // I then have to call /v3/videos with all video id's, and sum all the durations together
+  // Obviously this is a lot of time to process, so I guess I would have a load indicator or something, I wonder if I can use youtube's
+  asyncJsonGET(playlist_api_url, function(res) {
+    console.log("Playlist response:", res);
+    // TODO: Call GET /v3/videos to get video information
+    // TODO: Convert video objects to what the data variable looks like
+    // TODO: Render length :D
+    var data = ["PT32H10M33S", "PT2M01S", "PT32M10S", "PT11M5S","PT22M10S"];
+    length = renderLength(sumLengths(data), document.location.pathname === "/playlist" ? "long" : "short"))
+    callback(length);
+  }, function(err) {
+    console.error(err);
+  });
+};
+
+/**
+ * Renders a playlist length in the DOM
+ *
+ * @param {string} length - A readable length of the playlist
+ */
+function renderLengthInDOM(length) {
   var length_li = document.getElementById('pl-detail-length');
   if (length_li === null) {
     length_li = document.createElement('li');
@@ -130,20 +144,36 @@ function renderLengthInDOM(length, format_string) {
   playlist_details[0].appendChild(length_li);
 };
 
+/**
+ * Adds a playlist length to the DOM
+ *
+ * @param {string} length - A readable length of the playlist
+ */
+function addLengthToDOM(length) {
+  console.log("Adding length:" + length);
+  if (document.readyState === "interactive" || document.readyState === "complete"){
+    renderLengthInDOM(length);
+  } else {
+    function DOMLoadedHandler(){
+      renderLengthInDOM(length);
+      document.removeEventListener('DOMContentLoaded', DOMLoadedHandler);
+    };
+    document.addEventListener('DOMContentLoaded', DOMLoadedHandler);
+  };
+};
+
+var keys_URL = chrome.extension.getURL("keys.json");
+readTextFile(keys_URL, function(json) {
+  var keys = JSON.parse(json);
+  var list_regex = /(?:https?:\/\/)www\.youtube\.com\/(?:(?:playlist)|(?:watch))\?.*?(?:list=([A-z\d]+)).*/;
+  var url = document.location.href;
+  var list_id = url.match(list_regex)[1];
+  console.log("list id:",list_id);
+  getPlaylistLength(list_id, keys["YTDataAPIKey"], addLengthToDOM);
+});
+
 document.addEventListener('DOMContentLoaded', function main() {
-  var keys_URL = chrome.extension.getURL("keys.json");
-  readTextFile(keys_URL, function(json) {
-    var data = JSON.parse(json);
-    console.log(data);
-    var list_regex = /(?:https?:\/\/)www\.youtube.com\/(?:(?:playlist)|(?:watch))\?.*?(?:list=([^&\s]+)).*/g
-    var url = document.location.href;
-    var list_id = url.match(list_regex)[1]
-    getPlaylistLength(list_id, data["YTDataAPIKey"], function (response) {
-      var length = moment.duration("PT32H10M33S");
-      console.log("Calculated length:" + length);
-      renderLengthInDOM(length, document.location.pathname === "/playlist" ? "long" : "short");
-    });
-  });
+  document.removeEventListener('DOMContentLoaded');
   console.log("Script ran");
 });
 })(this);
