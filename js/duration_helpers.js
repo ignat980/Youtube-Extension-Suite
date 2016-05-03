bbbf0beb1c8f76015b72617a87cdb28a89238644
Ignat Remizov <ignat980@gmail.com>
2016-05-03 00:17:41 -0700

    Modularized code
    I don’t know if this is the correct way to do this going forward, but I
    think it should be ok

---
+++ js/duration_helpers.js
@@ -0,0 +1,48 @@
+;(function(root, undefined) {
+  /**
+   * Reduces a bunch of ISO 8601 durations into a moment.Duration object
+   *
+   * @param {[string]} data - An array of ISO 8601 formatted strings
+   * @returns {Duration} - The sum of the ISO 8601 strings as a moment.Duration object
+   */
+  function sumLengthsIntoDuration(data) {
+    console.log("Summing together strings");
+    return data.reduce((previous, current) => {
+      var duration = previous.contentDetails ? moment.duration(previous.contentDetails.duration) : previous;
+      duration.add(moment.duration(current.contentDetails.duration));
+      return duration;
+    });
+  };
+
+  /**
+   * Formats a moment.Duration object into a readable string
+   *
+   * @param {Duration} duration - a moment.Duration object that will be formatted
+   * @param {string} format_string - the string that will be used by .format() or you can use the two default values "long" or "short"
+   * @returns {string} - a formatted string from a Duration object using the moment-duration-format plugin
+   */
+  function formatDuration(duration, format_string) {
+    console.log("Formatting", duration);
+    var length;
+    if (format_string === "long") {
+      format = [
+        duration.years() === 1 ? "Year" : "Years",
+        duration.months() === 1 ? "Month" : "Months",
+        duration.days() === 1 ? "Day" : "Days",
+        duration.hours() === 1 ? "Hour" : "Hours",
+        duration.minutes() === 1 ? "Minute" : "Minutes",
+        duration.seconds() === 1 ? "Second" : "Seconds"
+      ];
+      length = duration.format("y [" + format[0] + "] M [" + format[1] + "] d [" + format[2] +
+      "] h [" + format[3] + "] m [" + format[4] + " and] s [" + format[5] + "]");
+    } else if (format_string === "short") {
+      length = duration.format("y[y] M[m] d[d] h:mm:ss");
+    } else {
+      length = duration.format(format_string);
+    };
+    return length;
+  }
+
+  root.sumLengthsIntoDuration = sumLengthsIntoDuration
+  root.formatDuration         = formatDuration
+})(this);
---
+++ js/helpers.js
@@ -0,0 +1,108 @@
+;(function(root, undefined) {
+  /**
+   * Adds formatting to strings, used like "Hello {0}!".format("World")
+   *
+   * @source https://stackoverflow.com/a/4673436/3923022 and https://stackoverflow.com/a/18234317/3923022
+   */
+  function addFormatStringFunction() {
+    if (!String.prototype.format) {
+      String.prototype.format = function() {
+        var args = typeof arguments[0] === 'object' ? arguments[0] : arguments;
+        return this.replace(/{(\d?[A-Za-z]?)}/gi, (match, arg) =>
+          typeof args[arg] != 'undefined' ? args[arg] : match)
+      };
+    }
+  }
+
+  /**
+   * Runs a for loop asynchronously, call the funtion passed to your loop when you want the loop to run again
+   *
+   * @param: {object} o - an object that has
+   *                        a 'length' property for how many times to iterate,
+   *                        a 'loop' property which is the iteration body,
+   *                        a 'callback' property that gets called at the end of the for loop
+   * @source https://stackoverflow.com/a/7654602/3923022
+   */
+  var AsyncLooper = function(o) {
+    this.i = -1;
+    this.length = o.i
+    this.o = o
+    this.loop();//start iterating
+  }
+
+  AsyncLooper.prototype.loop = function() {
+    this.i++;
+    if(this.i === this.length) {
+      if (this.o.callback) {
+        this.o.callback();
+      };
+      return;
+    };
+    this.o.loop(this.i);
+  };
+
+  /**
+   * @param {string} url - The URL to get from
+   * @param {function(Response)} callback - Called when the GET json request finishes
+   * @param {function(Error)} errorCallback - Called when the request fails or returns an error
+   */
+  function asyncJsonGET(url, callback, errorCallback) {
+    var x = new XMLHttpRequest();
+    x.open("GET", url);
+    // x.setRequestHeader("If-None-Match","etag")
+    x.responseType = 'json';
+    x.onload = function() {
+      if (x.status === 400 || x.status === 404) {
+        errorCallback(x);
+      } else if (!x.response) {
+        errorCallback("No response.");
+      } else if (x.response.error) {
+        errorCallback(x.response.error);
+      } else {
+        callback(x.response);
+      };
+    };
+    x.onerror = function() {
+      errorCallback('Network error.');
+    };
+    x.send(null);
+  };
+
+  /**
+   * Read a local json text file.
+   *
+   * @param {string} file - The filepath to the json resourse
+   * @param {function(string)} callback - called when the json file has been read.
+   * @source - http://stackoverflow.com/a/34579496/3923022
+   */
+
+  function readJsonFile(file, callback) {
+    var raw_file = new XMLHttpRequest();
+    raw_file.overrideMimeType("application/json");
+    raw_file.open("GET", file, true);
+    raw_file.onreadystatechange = function() {
+        if (raw_file.readyState === 4 && raw_file.status == "200") {
+            callback(raw_file.responseText);
+        };
+    };
+    raw_file.send(null);
+  };
+
+  /**
+   * Runs an external script
+   *
+   * @param {string} script - The filepath to the script to be run
+   */
+  function addLoader(script) {
+    var script_element = document.createElement('script');
+    script_element.type = 'text/javascript';
+    script_element.src = chrome.extension.getURL(script);
+    document.head.appendChild(spinner_script);
+  };
+
+  root.addFormatStringFunction = addFormatStringFunction
+  root.AsyncLooper             = AsyncLooper
+  root.asyncJsonGET            = asyncJsonGET
+  root.readJsonFile            = readJsonFile
+  root.addLoader               = addLoader
+})(this);
